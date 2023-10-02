f259c54c741baa0332743458d5e0bea83e8ff0e9
Abhinav Maurya <114284694+abhinav2-3@users.noreply.github.com>
2023-10-02 14:21:46 +0530

    Update Login.jsx

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     };
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/login",
+        "https://foodhut-server.onrender.com/login",
         formData,
         {
           headers: {
