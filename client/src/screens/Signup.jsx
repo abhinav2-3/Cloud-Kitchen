7548e7f5c87e272b48a3ddc4c529fe690dc93bb2
Abhinav Maurya <114284694+abhinav2-3@users.noreply.github.com>
2023-10-02 14:23:17 +0530

    Update Signup.jsx

--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/signup",
+        "https://foodhut-server.onrender.com/signup",
         formData,
         {
           headers: {
